a3963802dcebb93578f642d6659b3a1e954aaea9
keynesGitHub <ketteirng77@gmail.com>
2020-09-22 20:21:40 +0800

    add fuc

---
+++ fuc_TXD1.c
@@ -0,0 +1,42 @@
+
+
+
+#include "NUC100Series.h"
+#include "globalVariable.h"
+#include "fuc_modbusCRC16.h"
+
+void t1_fillIn_content(uint8_t lu8_devID,   uint8_t lu8_fc,   uint16_t lu16_sa,   uint16_t lu16_nbr)
+{
+	  uint16_t i;
+	  uint16_t pt = 3;//inital 3
+	  uint16_t st;
+	
+    TXD1.gu8A_tramtData[0] = lu8_devID;
+	  TXD1.gu8A_tramtData[1] = lu8_fc;
+	  TXD1.gu8A_tramtData[2] = (uint8_t)(lu16_nbr * 2);
+	
+	  //===hold register fill in===
+	  for(i=0; i<lu16_nbr; i++) 
+	  {
+		    TXD1.gu8A_tramtData[pt++] = (uint8_t)((MB_procl.gu16_holdregsr[lu16_sa] & 0xFF00) >> 8);
+			  TXD1.gu8A_tramtData[pt++] = (uint8_t)(MB_procl.gu16_holdregsr[lu16_sa++] & 0x00FF);
+		}
+		//===
+		
+		//===CRC16===
+		for(st=0; st<pt; st++)
+		{
+		    TXD1.gu8A_MB_CRC16Content[st] = TXD1.gu8A_tramtData[st];
+		}
+		TXD1.gu16_MB_CRC16Value = ModBus_CRC16(TXD1.gu8A_MB_CRC16Content, pt);
+		//===
+		
+		TXD1.gu8A_tramtData[pt++] = (TXD1.gu16_MB_CRC16Value & 0xFF00) >> 8;
+		TXD1.gu8A_tramtData[pt++] = TXD1.gu16_MB_CRC16Value & 0x00FF;
+		
+		TXD1.gu16_tramtEnd = pt;
+		TXD1.gu16_tramtIndex = 0;
+	  
+}
+
+
---
+++ fuc_TXD1.h
@@ -0,0 +1,8 @@
+
+
+#include "NUC100Series.h"
+
+void t1_fillIn_content(uint8_t, uint8_t, uint8_t, uint16_t);
+
+
+ 
